0898f70d2d826901b15614011b556f1c0e3de3e4
Tài Lê Nguyễn Thắng <lenthangtai@gmail.com>
2022-09-20 08:54:13 +0700

    Add project files.

---
+++ ToolEntrySHD/frmToolEntrySHD.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace ToolEntrySHD
+{
+    partial class frmToolEntrySHD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInputPath = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.ImageView = new System.Windows.Forms.PictureBox();
+            this.lblValueIndex = new System.Windows.Forms.Label();
+            this.txtEntry = new System.Windows.Forms.TextBox();
+            this.txtPathHidden = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnInputPath
+            // 
+            this.btnInputPath.Location = new System.Drawing.Point(12, 10);
+            this.btnInputPath.Name = "btnInputPath";
+            this.btnInputPath.Size = new System.Drawing.Size(49, 23);
+            this.btnInputPath.TabIndex = 0;
+            this.btnInputPath.Text = "....";
+            this.btnInputPath.UseVisualStyleBackColor = true;
+            this.btnInputPath.Click += new System.EventHandler(this.btnInputPath_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(69, 13);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(363, 20);
+            this.txtPath.TabIndex = 3;
+            this.txtPath.TabStop = false;
+            // 
+            // ImageView
+            // 
+            this.ImageView.Location = new System.Drawing.Point(13, 42);
+            this.ImageView.Name = "ImageView";
+            this.ImageView.Size = new System.Drawing.Size(418, 477);
+            this.ImageView.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ImageView.TabIndex = 2;
+            this.ImageView.TabStop = false;
+            // 
+            // lblValueIndex
+            // 
+            this.lblValueIndex.AutoSize = true;
+            this.lblValueIndex.Location = new System.Drawing.Point(396, 527);
+            this.lblValueIndex.Name = "lblValueIndex";
+            this.lblValueIndex.Size = new System.Drawing.Size(12, 13);
+            this.lblValueIndex.TabIndex = 3;
+            this.lblValueIndex.Text = "/";
+            // 
+            // txtEntry
+            // 
+            this.txtEntry.Location = new System.Drawing.Point(13, 555);
+            this.txtEntry.Name = "txtEntry";
+            this.txtEntry.Size = new System.Drawing.Size(418, 20);
+            this.txtEntry.TabIndex = 1;
+            this.txtEntry.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtEntry_KeyDown);
+            // 
+            // txtPathHidden
+            // 
+            this.txtPathHidden.Location = new System.Drawing.Point(421, 13);
+            this.txtPathHidden.Name = "txtPathHidden";
+            this.txtPathHidden.ReadOnly = true;
+            this.txtPathHidden.Size = new System.Drawing.Size(10, 20);
+            this.txtPathHidden.TabIndex = 4;
+            this.txtPathHidden.TabStop = false;
+            this.txtPathHidden.Visible = false;
+            // 
+            // frmToolEntrySHD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 583);
+            this.Controls.Add(this.txtPathHidden);
+            this.Controls.Add(this.txtEntry);
+            this.Controls.Add(this.lblValueIndex);
+            this.Controls.Add(this.ImageView);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnInputPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.Name = "frmToolEntrySHD";
+            this.Load += new System.EventHandler(this.frmToolEntrySHD_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmToolEntrySHD_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.ImageView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInputPath;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.PictureBox ImageView;
+        private System.Windows.Forms.Label lblValueIndex;
+        private System.Windows.Forms.TextBox txtEntry;
+        private System.Windows.Forms.TextBox txtPathHidden;
+    }
+}
+
---
+++ ToolEntrySHD/frmToolEntrySHD.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToolEntrySHD
+{
+    public partial class frmToolEntrySHD : Form
+    {
+        public frmToolEntrySHD()
+        {
+            InitializeComponent();
+        }
+        List<string> locFile = new List<string>();
+        List<string> FileResult = new List<string>();
+        FolderBrowserDialog fd = new FolderBrowserDialog();
+        //string[] filterFile = { "*.png", "*.jpg", "*.gif" };
+        int counter = 0;
+        private void showImage()
+        {
+           
+            int counterResult = FileResult.Count();
+            if (counterResult != 0)
+            {                   
+                ImageView.Image = Image.FromFile(locFile[counterResult]);
+                //show giá trị index trong list / tổng giá trị index đếm được trong folder
+                lblValueIndex.Text = Convert.ToInt32(counterResult + 1) + "/" + locFile.Count.ToString();
+                txtEntry.Clear();
+
+            }
+            else  if(counterResult == 0)
+            {
+                ImageView.Image = Image.FromFile(locFile[counter]);
+                //show giá trị index trong list / tổng giá trị index đếm được trong folder
+                lblValueIndex.Text = Convert.ToInt32(counter + 1) + "/" + locFile.Count.ToString();
+                txtEntry.Clear();
+            }
+        }
+        private void frmToolEntrySHD_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                fd.ShowDialog();
+                //string filepath = fd.SelectedPath;
+                locFile = Directory.GetFiles(fd.SelectedPath, "*.jpg", SearchOption.TopDirectoryOnly).ToList();
+                txtPathHidden.Text = fd.SelectedPath.ToString();
+                txtPath.Text = fd.SelectedPath.ToString();
+                if (locFile.Count() == 0)
+                {
+                    MessageBox.Show("This Folder Don't Have A Picture!!!\nPlease, Pick Folder Another.", "Alert Title", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    DirectoryInfo dir = new DirectoryInfo(fd.SelectedPath + "\\result");
+                    if (!dir.Exists)
+                    {
+                        dir.Create();
+                        FileResult = Directory.GetFiles(fd.SelectedPath + "\\result", "*.jpg", SearchOption.TopDirectoryOnly).ToList();
+                        showImage();
+                    }
+                    else
+                    {
+                        FileResult = Directory.GetFiles(fd.SelectedPath + "\\result", "*.jpg", SearchOption.TopDirectoryOnly).ToList();
+                        showImage();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnInputPath_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                fd.ShowDialog();
+                locFile = Directory.GetFiles(fd.SelectedPath, "*.jpg", SearchOption.TopDirectoryOnly).ToList();
+                txtPath.Text = fd.SelectedPath.ToString();
+
+                if (locFile.Count() == 0)
+                {
+                    MessageBox.Show("This Folder Don't Have A Picture!!!\nPlease, Pick Folder Another.", "Alert Title", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    txtEntry.Focus();
+                    showImage();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+
+        private void txtEntry_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    int counterResult = FileResult.Count();
+                    if (counterResult != 0)
+                    {
+                        ImageView.Image.Save(txtPathHidden.Text + "\\result\\" + (counterResult + 1).ToString().PadLeft(6, '0') + "_" + txtEntry.Text.Trim() + ".jpg");
+                    }
+                    else if (counterResult == 0)
+                    {
+                        ImageView.Image.Save(txtPathHidden.Text + "\\result\\" + (counter + 1).ToString().PadLeft(6, '0') + "_" + txtEntry.Text.Trim() + ".jpg");
+                    }
+                    counter += 1;
+                    counterResult += 1;
+                    if (counter == locFile.Count || counterResult == locFile.Count)
+                    {
+                        DialogResult drs = MessageBox.Show("Complete!", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                        if (drs = DialogResult.OK)
+                        {
+                            this.Close();
+                        }
+                        return;
+                    }
+                    else
+                    {
+                        FileResult = Directory.GetFiles(txtPathHidden.Text + "\\result\\", "*.jpg", SearchOption.TopDirectoryOnly).ToList();
+                        showImage();
+                    }
+                }
+            }
+            catch (Exception ex)
+            { MessageBox.Show(ex.Message);}
+        }
+        private void frmToolEntrySHD_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                switch (e.KeyData)
+                {
+                    case Keys.Escape:
+                        DialogResult dg = MessageBox.Show("Bạn muốn thoát hệ thống.!", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                        if (dg == DialogResult.OK)
+                        {
+                            this.Close();
+                        }
+                        break;
+                    case Keys.Delete:
+                        counter += 1;
+                        if (counter == locFile.Count)
+                        {
+                            ImageView.Image.Dispose();
+                            ImageView.Dispose();
+                            File.Delete(locFile[counter - 1]);
+                            MessageBox.Show("Complete!!", "Thông báo", MessageBoxButtons.OK);
+                            this.Close();
+                        }
+                        else
+                        {
+                            ImageView.Image.Dispose();
+                            File.Delete(locFile[counter - 1]);
+                            showImage();
+                        }
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
