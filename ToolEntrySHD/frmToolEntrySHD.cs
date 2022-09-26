6fcad66c7ea649997c32997151d4415f2eabf4a4
Tài Nhớt <89625796+lenthangtai@users.noreply.github.com>
2022-09-26 09:18:23 +0700

    Update frmToolEntrySHD.cs

--- ToolEntrySHD/frmToolEntrySHD.cs
+++ ToolEntrySHD/frmToolEntrySHD.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,30 +26,16 @@ namespace ToolEntrySHD
         int counter = 0;
         private void showImage()
         {
-           
-            int counterResult = FileResult.Count();
-            if (counterResult != 0)
-            {                   
-                ImageView.Image = Image.FromFile(locFile[counterResult]);
-                //show giá trị index trong list / tổng giá trị index đếm được trong folder
-                lblValueIndex.Text = Convert.ToInt32(counterResult + 1) + "/" + locFile.Count.ToString();
-                txtEntry.Clear();
-
-            }
-            else  if(counterResult == 0)
-            {
-                ImageView.Image = Image.FromFile(locFile[counter]);
-                //show giá trị index trong list / tổng giá trị index đếm được trong folder
-                lblValueIndex.Text = Convert.ToInt32(counter + 1) + "/" + locFile.Count.ToString();
-                txtEntry.Clear();
-            }
+            counter = FileResult.Count();
+            ImageView.Image = Image.FromFile(locFile[counter]);
+            lblValueIndex.Text = Convert.ToInt32(counter + 1) + "/" + locFile.Count.ToString();
+            txtEntry.Clear();
         }
         private void frmToolEntrySHD_Load(object sender, EventArgs e)
         {
             try
             {
                 fd.ShowDialog();
-                //string filepath = fd.SelectedPath;
                 locFile = Directory.GetFiles(fd.SelectedPath, "*.jpg", SearchOption.TopDirectoryOnly).ToList();
                 txtPathHidden.Text = fd.SelectedPath.ToString();
                 txtPath.Text = fd.SelectedPath.ToString();
@@ -79,52 +66,40 @@ namespace ToolEntrySHD
         }
         private void btnInputPath_Click(object sender, EventArgs e)
         {
+
             try
             {
                 fd.ShowDialog();
                 locFile = Directory.GetFiles(fd.SelectedPath, "*.jpg", SearchOption.TopDirectoryOnly).ToList();
                 txtPath.Text = fd.SelectedPath.ToString();
-
                 if (locFile.Count() == 0)
                 {
                     MessageBox.Show("This Folder Don't Have A Picture!!!\nPlease, Pick Folder Another.", "Alert Title", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
                 else
                 {
-                    txtEntry.Focus();
                     showImage();
+                    txtEntry.Focus();
                 }
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
-
-
         }
-
-
         private void txtEntry_KeyDown(object sender, KeyEventArgs e)
         {
             try
             {
                 if (e.KeyCode == Keys.Enter)
                 {
-                    int counterResult = FileResult.Count();
-                    if (counterResult != 0)
-                    {
-                        ImageView.Image.Save(txtPathHidden.Text + "\\result\\" + (counterResult + 1).ToString().PadLeft(6, '0') + "_" + txtEntry.Text.Trim() + ".jpg");
-                    }
-                    else if (counterResult == 0)
-                    {
-                        ImageView.Image.Save(txtPathHidden.Text + "\\result\\" + (counter + 1).ToString().PadLeft(6, '0') + "_" + txtEntry.Text.Trim() + ".jpg");
-                    }
+                    counter = FileResult.Count();
+                    ImageView.Image.Save(txtPathHidden.Text + "\\result\\" + (counter + 1).ToString().PadLeft(6, '0') + "_" + txtEntry.Text.Trim() + ".jpg");
                     counter += 1;
-                    counterResult += 1;
-                    if (counter == locFile.Count || counterResult == locFile.Count)
+                    if (counter == locFile.Count)
                     {
                         DialogResult drs = MessageBox.Show("Complete!", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
-                        if (drs = DialogResult.OK)
+                        if (drs == DialogResult.OK)
                         {
                             this.Close();
                         }
@@ -134,11 +109,12 @@ namespace ToolEntrySHD
                     {
                         FileResult = Directory.GetFiles(txtPathHidden.Text + "\\result\\", "*.jpg", SearchOption.TopDirectoryOnly).ToList();
                         showImage();
+
                     }
                 }
             }
             catch (Exception ex)
-            { MessageBox.Show(ex.Message);}
+            { MessageBox.Show(ex.Message); }
         }
         private void frmToolEntrySHD_KeyDown(object sender, KeyEventArgs e)
         {
